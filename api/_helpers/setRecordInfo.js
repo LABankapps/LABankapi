d83b633b5c27c2219ff6e9d47c5466a3900f75fc
FabienGreard <grd.fabien@gmail.com>
2018-01-23 19:42:41 +0100

    fix setRecord

--- api/_helpers/setRecordInfo.js
+++ api/_helpers/setRecordInfo.js
@@ -3,9 +3,17 @@ const Engine = require('../models/engine');
 
 exports.setRecordInfo = function setRecordInfo(record, ...callback){
   User.findById(record.from, (err, user) => {
-    record.from = user.email;
+    if(user !== null){
+      record.from = user.email;
+    }else{
+      record.from = 'Utilisateur inconnu';
+    }
     Engine.findById(record.engine, (err, engine) => {
-       record.engine = engine.name;
+      if(engine !== null){
+        record.engine = engine.name;
+      }else{
+        record.engine = 'Machine inconnu';
+      }
        (() => {
          if(callback.length > 0){
            callback[0]();
