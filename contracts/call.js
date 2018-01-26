c089344f8632b4c585969e73dd59b729dbc9face
Giraudo Nicolas <giraudo.nicolas13@gmail.com>
2018-01-26 11:45:06 +0100

    test connection geth

---
+++ contracts/call.js
@@ -0,0 +1,12 @@
+const Web3 = require('web3');
+const compiledContract = require('./LABankdapp');
+const contractAddress = "0xcc08a260b9bf4887c959f2eec6dc20286325a1c3"; //returned after deploy
+const nullAddress = "0x0000000000000000000000000000000000000000";
+const localhost = "http://localhost:8545";
+const web3 = new Web3(new Web3.providers.HttpProvider(localhost));
+const Contract = web3.eth.contract(compiledContract.abi);
+const Labank = Contract.at(contractAddress); // instantiate by address
+const coinbase = web3.eth.coinbase; //get eth.defaultAccount (synchronously)
+
+console.log(coinbase);
+console.log(web3.fomWei(web3.eth.getBalance(web3.eth.coinbase), "ether"));
