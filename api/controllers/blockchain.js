53a3f7f906d7ba04dd10132024b113816c2e02bd
Giraudo Nicolas <giraudo.nicolas13@gmail.com>
2018-02-09 01:42:32 +0100

    consolelog

--- api/controllers/blockchain.js
+++ api/controllers/blockchain.js
@@ -1,6 +1,6 @@
 const Web3 = require('web3');
 const compiledContract = require('../../contracts/LABankdapp');
-const contractAddress = "0xeaae0e3fd4552fab7757c465658ea82c7dd7baf5"; //returned after deploy
+const contractAddress = "0xcc08a260b9bf4887c959f2eec6dc20286325a1c3"; //returned after deploy
 const nullAddress = "0x0000000000000000000000000000000000000000";
 const localhost = "http://localhost:8545";
 const web3 = new Web3(new Web3.providers.HttpProvider(localhost));
